94c9fd235015d6b4e72e8f07fd1f840544366ed5
Rishabh0603 <rishabhtrivedilko.50@gmail.com>
2023-02-19 14:14:22 +0530

    Hypotenuse Js part done
    Hypotenuse Web Page is ready for calculating the hypotenuse of a triangle

---
+++ Hypotenuse.js
@@ -0,0 +1,20 @@
+var base = document.querySelector("#base");
+var height = document.querySelector("#height")
+var button1 = document.querySelector("#button");
+var output = document.querySelector("#output");
+
+
+function controlEventListener(){
+    
+    var base_value = base.value;
+    var height_value = height.value;
+    base_value = parseFloat(base_value);
+     height_value = parseFloat(height_value);
+     if(base_value>0 && height_value >0){
+    var hypotenuse = Math.sqrt(Math.pow(base_value,2) + Math.pow(height_value,2));
+    output.innerText = "The length of hypotenuse is " + hypotenuse;}
+    else
+    output.innerText="Please enter correct value";
+
+}
+button.addEventListener("click",controlEventListener);
